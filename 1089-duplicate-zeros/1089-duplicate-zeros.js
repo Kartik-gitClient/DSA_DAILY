da4e4523dd4b343e79cdb336d7d1be8fe3c3a9f6
Kartik-khandelwal <167613825+Kartik-gitClient@users.noreply.github.com>
2026-08-07 22:51:23 +0530

    Time: 3 ms (60.41%), Space: 55.3 MB (90.44%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    let Lptr = 0;
+
+    for(let i =0; i < arr.length ; i++ ){
+        if(arr[i] == 0){
+            arr.pop();
+            arr.splice(i+1, 0, 0);
+            i++
+        }
+    }
+    
+};
