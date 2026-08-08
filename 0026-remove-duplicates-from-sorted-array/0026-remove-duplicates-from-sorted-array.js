e91771a017fb076ea99e01a51452e43c74323669
Kartik-khandelwal <167613825+Kartik-gitClient@users.noreply.github.com>
2026-08-08 19:15:23 +0530

    Time: 0 ms (100%), Space: 58.9 MB (7.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,22 +4,14 @@
  */
 var removeDuplicates = function (nums) {
     let Lptr = 0;
-    // let Rptr = 0;
 
     for(let Rptr = 0; Rptr < nums.length ; Rptr++){
-
-        if(nums[Rptr] != nums[Rptr -1]){
-            nums[Lptr] = nums[Rptr]
+        if(nums[Rptr] != nums[Rptr-1]){
+            nums[Lptr] = nums[Rptr];
             Lptr++
         }
-        // if(nums[Rptr] == nums[Rptr -1]){
-        //     // Lptr++
-        //     // Rptr++
-        // }else{
-        //     nums[Lptr] = nums[Rptr]
-        //     Lptr++
-        // }
     }
-    return Lptr 
+
+    return Lptr
 
 };
