49208712ab326fb82a3dcab59e64613f7faec3ad
Kartik-khandelwal <167613825+Kartik-gitClient@users.noreply.github.com>
2026-08-13 11:16:25 +0530

    Time: 102 ms (5.13%), Space: 64.9 MB (5.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,14 +4,45 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+    function quicksort(array , low ,high){
+        if(low >= high){
+            return;
+        }
+        let pivot = partition(array , low , high);
+        quicksort(array , low , pivot - 1);
+        quicksort(array , pivot + 1 , high)
+    }
+
+    function partition(array , low , high){
+        let pivot = array[high];
+        let i = low - 1;
+
+        for(let j = low ; j<= high - 1 ; j++){
+            if(array[j] < pivot){
+                i++;
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        let temp = array[high];
+        array[high] = array[i];
+        array[i] = temp;
+
+        return i;
+    }
     let merged = nums1.concat(nums2);
-    let sorted = merged.sort((a,b) => a-b);
+    quicksort(merged , 0 , merged.length- 1);
     let len = merged.length
+    console.log(merged);
     console.log(len)
     if(len % 2 == 0){
+
         return (merged[parseInt(len/2)] + merged[parseInt(len/2 - 1)]) / 2;
     }else{
         return merged[parseInt(len/2)];
     }
+
 
 };
